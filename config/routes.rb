d79102a949c8bf1b782e17f24643acd8d2412bf5
root <yusukeabe000@gmail.com>
2025-03-03 19:00:43 +0900

    fix: Tailwindのキャッシュを削除

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,8 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
+  Rails.application.routes.draw do
+    root "tops#index"
+  end
   # root "posts#index"
 end
