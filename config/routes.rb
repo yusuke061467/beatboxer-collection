a3eebdcb4c8f612d9988c24ef57b13df8447145d
unknown <yusukeabe000@gmail.com>
2025-03-16 12:52:05 +0900

    delete: topsコントローラーとindex.html.erbを削除

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root to: "tops#index"
+  root to: "beatboxers#index"
   # root "posts#index"
+
+  resources :users, only: %i[new create]
 end
