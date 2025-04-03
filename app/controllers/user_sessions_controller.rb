40f8d8ed86443e38c3648df541cd044540736732
unknown <yusukeabe000@gmail.com>
2025-04-03 09:22:11 +0900

    add: ログイン画面に関するフラッシュメッセージの実装

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -6,14 +6,17 @@ class UserSessionsController < ApplicationController
     def create
       @user = login(params[:email], params[:password])
       if @user
+        flash[:notice] = 'ログインしました'
         redirect_back_or_to root_path
       else
+        flash.now[:alert] = 'ログインに失敗しました'
         render :new
       end
     end
     
     def destroy
       logout
+      flash[:notice] = 'ログアウトしました'
       redirect_to root_path
     end
    end
