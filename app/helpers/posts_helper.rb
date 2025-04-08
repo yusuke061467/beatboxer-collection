f7674aab37ab4f9c3697ca6dc83f98ce06b218b2
root <yusukeabe000@gmail.com>
2025-04-08 18:33:31 +0900

    fix: _postのyoutube画面の改善

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,10 +1,10 @@
 module PostsHelper
-    def find_youtube_url(youtube_url)
-      if youtube_url.include?("https://youtu.be/")
-        youtube_url.strip!.gsub("https://youtu.be/", "")
+    def find_youtube_url(youtube_video)
+      if youtube_video.include?("https://youtu.be/")
+        youtube_video.gsub("https://youtu.be/", "")
         # "https://youtu.be/WGiUk8VakxQ" 11桁のyoutubeのURLが出力されるようにする
-      else
-        youtube_url.strip!.gsub("https://www.youtube.com/watch?v=", "")
+      else youtube_video.include?("https://www.youtube.com/watch?v=")
+        youtube_video.gsub("https://www.youtube.com/watch?v=", "")
         # "https://www.youtube.com/watch?v=WGiUk8VakxQ" 11桁のyoutubeのURLが出力されるようにする
       end
     end
