ba0ca7c9da45fc8deda42629ba906f2756d456c5
unknown <yusukeabe000@gmail.com>
2025-04-10 14:21:57 +0900

    add: 投稿一覧機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root to: "beatboxers#index"
+  root to: "posts#index"
   # root "posts#index"
 
   resources :users, only: %i[new create destroy edit update]
