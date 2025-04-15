ead1f92df7b375cd2b2ecbfc49f732ce89c475e2
unknown <yusukeabe000@gmail.com>
2025-04-15 21:30:58 +0900

    add: 詳細画面のコンテンツ部分を作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,8 @@ class PostsController < ApplicationController
 
     def show
         @post = Post.find(params[:id])
+        @comment = Comment.new
+        @comments = @post.comments
     end
 
     def new
