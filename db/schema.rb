5c1d158ac7afc2b75423ed8516a2893c9b36c0b7
unknown <yusukeabe000@gmail.com>
2025-05-10 20:16:08 +0900

    add: sorceryのサブモジュールとしてuser_activationを追加し、db:migrateを実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_18_115028) do
+ActiveRecord::Schema[7.2].define(version: 2025_05_10_104821) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +65,10 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_18_115028) do
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
     t.string "avatar"
+    t.string "activation_state"
+    t.string "activation_token"
+    t.datetime "activation_token_expires_at"
+    t.index ["activation_token"], name: "index_users_on_activation_token"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
